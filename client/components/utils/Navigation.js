d5b08cb11ddc0d0cef990507c420aaee65399ad5
mmihevc <mmihevc@rams.colostate.edu>
2021-05-02 16:06:20 -0600

    box and picture

--- client/components/utils/Navigation.js
+++ client/components/utils/Navigation.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import {Avatar, Box, Typography} from "@material-ui/core";
+import {Box} from "@material-ui/core";
 import Logo from '../../static/images/logo.png'
 
 function Navigation() {
     return (
-        <Box style={{bottom: 0, position: 'absolute'}}>
-            <img src={Logo}/>
-        </Box>
+        <>
+            <Box>
+
+            </Box>
+            <Box style={{bottom: 0, position: 'absolute'}}>
+                <img src={Logo}/>
+            </Box>
+        </>
     )
 }
 
